789e1301aa8ff7113141df81ef030fed82e4bbca
HoRuiEn <ernie.birdy.ho@gmail.com>
2020-02-21 16:01:20 +0800

    Add image/mask height/width parameters to customize cutoff

--- dataset/voc_aug.py
+++ dataset/voc_aug.py
@@ -6,7 +6,13 @@ from torch.utils.data import Dataset
 
 
 class VOCAugDataSet(Dataset):
-    def __init__(self, dataset_path='/home/whizz/Desktop/ERFNet/list', data_list='train', transform=None):
+    def __init__(self, 
+                dataset_path='/home/whizz/Desktop/ERFNet/list', 
+                data_list='train', 
+                transform=None,
+                image_height=590,
+                image_width=1640,
+                is_testing=False):
 
         with open(os.path.join(dataset_path, data_list + '.txt')) as f:
             self.img_list = []
@@ -18,11 +24,12 @@ class VOCAugDataSet(Dataset):
                 self.img_list.append(dataset_path + line.strip().split(" ")[0])
                 self.label_list.append(dataset_path + line.strip().split(" ")[1])
                 self.exist_list.append(np.array([int(line.strip().split(" ")[2]), int(line.strip().split(" ")[3]), int(line.strip().split(" ")[4]), int(line.strip().split(" ")[5])]))
-
+        
+        self.image_cutoff = int(image_height - image_width / (1640/350))
         self.img_path = dataset_path
         self.gt_path = dataset_path
         self.transform = transform
-        self.is_testing = data_list == 'test_img' # 'val'
+        self.is_testing = is_testing
 
     def __len__(self):
         return len(self.img_list)
@@ -31,13 +38,16 @@ class VOCAugDataSet(Dataset):
         image = cv2.imread(os.path.join(self.img_path, self.img_list[idx])).astype(np.float32)
         label = cv2.imread(os.path.join(self.gt_path, self.label_list[idx]), cv2.IMREAD_UNCHANGED)
         exist = self.exist_list[idx]
-        image = image[240:, :, :]
+        
+        image = image[self.image_cutoff:, :, :]
         label = label[240:, :]
         label = label.squeeze()
+    
         if self.transform:
-            image, label = self.transform((image, label))
+            image, label = self.transform((image, label))            
             image = torch.from_numpy(image).permute(2, 0, 1).contiguous().float()
             label = torch.from_numpy(label).contiguous().long()
+        
         if self.is_testing:
             return image, label, self.img[idx]
         else:
--- options/options.py
+++ options/options.py
@@ -2,6 +2,10 @@ import argparse
 parser = argparse.ArgumentParser(description="PyTorch implementation of Semantic Segmentation")
 
 parser.add_argument('dataset', type=str, choices=['VOCAug', 'VOC2012', 'COCO', 'Cityscapes', 'ApolloScape', 'CULane'])
+parser.add_argument('--dataset_path', type=str, required=True)
+parser.add_argument('--image_height', type=int, default=590)
+parser.add_argument('--image_width', type=int, default=1640)
+parser.add_argument('--naming_format', type=str, default='.jpg')
 parser.add_argument('method', type=str, choices=['FCN', 'DeepLab', 'DeepLab3', 'PSPNet', 'ERFNet'])
 parser.add_argument('train_list', type=str)
 parser.add_argument('val_list', type=str)
@@ -11,8 +15,8 @@ parser.add_argument('--arch', type=str, default="resnet101")
 parser.add_argument('--dropout', '--do', default=0.1, type=float, metavar='DO', help='dropout ratio (default: 0.1)')
 parser.add_argument('--train_size', default=840, type=int, metavar='L', help='size of training patches (default: 473)')
 parser.add_argument('--test_size', default=840, type=int, metavar='L', help='size of testing patches (default: 513)')
-parser.add_argument('--img_height', default=208, type=int, metavar='L', help='height of input images (default: 208)')
-parser.add_argument('--img_width', default=976, type=int, metavar='L', help='width of input images (default: 976)')
+parser.add_argument('--mask_height', default=208, type=int, metavar='L', help='height of input images (default: 208)')
+parser.add_argument('--mask_width', default=976, type=int, metavar='L', help='width of input images (default: 976)')
 parser.add_argument('--local_rank', type=int, default=0) # distributed data parallel
 
 # ========================= Learning Configs ==========================
