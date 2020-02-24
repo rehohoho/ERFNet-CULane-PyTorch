825c932e44f4648554b22fcef58b5cf926b18b0d
HoRuiEn <ernie.birdy.ho@gmail.com>
2020-02-24 14:59:48 +0800

    To visualiser, Add parameters for variable crop and image size

--- tools/prob2lines/get_lane.py
+++ tools/prob2lines/get_lane.py
@@ -3,6 +3,7 @@
 import os
 import numpy as np
 import cv2
+from math import ceil
 import argparse
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -12,7 +13,12 @@ from matplotlib.figure import Figure
 class prob_to_lane:
 
 
-    def __init__(self, dataRoot, probRoot, img_hw = (590, 1640), mask_hw = (208, 976), n_points=18, thres=0.3):
+    def __init__(self, dataRoot, probRoot, 
+                img_hw = (590, 1640), 
+                mask_hw = (208, 976), 
+                height_from_bottom = 0.59322,
+                n_points=18, 
+                thres=0.3):
 
         self.dataRoot = dataRoot
         self.probRoot = probRoot
@@ -26,7 +32,8 @@ class prob_to_lane:
         self.thres = thres
 
         self.plot_type = ['r-', 'g-', 'b-', 'y-']
-
+        self.cropped_height = height_from_bottom * self.img_height
+        self.y_step = ceil(self.cropped_height / self.n_points)
 
     def get_lane(self, score_map):
 
@@ -34,7 +41,7 @@ class prob_to_lane:
 
         for i in range(self.n_points):
             
-            line_id = int(self.mask_height - i*20 / 350*self.mask_height) - 1
+            line_id = int(self.mask_height - i*self.y_step / self.cropped_height*self.mask_height) - 1
             line = score_map[line_id, :, 0]
 
             max_id = np.argmax(line)
@@ -89,13 +96,13 @@ class prob_to_lane:
             
             for j in range(self.n_points - 1):
 
-                axes.plot( [line[j],line[j+1]], [y_coord, y_coord-20], self.plot_type[i])
-                y_coord -= 20
+                axes.plot( [line[j],line[j+1]], [y_coord, y_coord - self.y_step], self.plot_type[i])
+                y_coord -= self.y_step
 
         axes.axis('off')
         canvas.draw()
         image = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')
-        image = image.reshape(self.img_height, self.img_width-1, 3)
+        image = image.reshape(self.img_height, self.img_width, 3)
         cv2.imshow('Lane_Detection', image)
 
 
@@ -123,6 +130,11 @@ if __name__ == '__main__':
         type=str,
         help='path to txt file with paths to images to test'
     )
+    parser.add_argument('--image_height', type=int, default=590)
+    parser.add_argument('--image_width', type=int, default=1640)
+    parser.add_argument('--mask_height', default=208, type=int, metavar='L', help='height of input images (default: 208)')
+    parser.add_argument('--mask_width', default=976, type=int, metavar='L', help='width of input images (default: 976)')
+    parser.add_argument('--height_from_bottom', type=float, default=0.59322)
 
     args = parser.parse_args()
 
@@ -131,7 +143,9 @@ if __name__ == '__main__':
     image_list = [ line.split(' ')[0] for line in image_list ]
     image_list_len = len(image_list)
 
-    lane_converter = prob_to_lane(args.data_root, args.prob_root)
+    lane_converter = prob_to_lane(args.data_root, args.prob_root,
+                        img_hw = (args.image_height, args.image_width),
+                        mask_hw = (args.mask_height, args.mask_width))
 
     for i in range(image_list_len):
 
--- tools/prob2lines/get_lane.sh
+++ tools/prob2lines/get_lane.sh
@@ -1,8 +1,11 @@
 
-ERFNET_FOLDER="C:/Users/User/Desktop/ERFNet"
+ERFNET_FOLDER="C:\\Users\\User\\Desktop\\ERFNet"
 
 python get_lane.py \
-    --data_root="${ERFNET_FOLDER}/list/" \
-    --prob_root="${ERFNET_FOLDER}/predicts/vgg_SCNN_DULR_w9" \
-    --output_root="${ERFNET_FOLDER}/tools/output/vgg_SCNN_DULR_w9" \
-    --test_list="${ERFNET_FOLDER}/list/scooter_test_set.txt"
+    --data_root="${ERFNET_FOLDER}\\list\\" \
+    --prob_root="${ERFNET_FOLDER}\\predicts\\vgg_SCNN_DULR_w9" \
+    --output_root="${ERFNET_FOLDER}\\tools\\output\\vgg_SCNN_DULR_w9" \
+    --test_list="${ERFNET_FOLDER}\\list\\tusimple_0531.txt" \
+    --height_from_bottom=0.7 \
+    --image_height=720 \
+    --image_width=1280
